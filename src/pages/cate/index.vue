28c6af1734be7a6507ce3c182b1b28707c272ff1
xiosn <2540156637@qq.com>
2022-07-05 10:23:48 +0800

    基础模板

---
+++ src/pages/cate/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="cate">cate</div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped></style>
---
+++ src/pages/haha/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="haha">haha</div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped></style>
